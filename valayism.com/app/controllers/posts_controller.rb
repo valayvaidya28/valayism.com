f186c5fc13f291a6255d661971d0c0dc321d1deb
valayvaidya28 <valayvaidya28@gmail.com>
2014-09-04 18:15:55 +0530

    Add Dashboard/Create/Edit features to the blog.

--- valayism.com/app/controllers/posts_controller.rb
+++ valayism.com/app/controllers/posts_controller.rb
@@ -7,21 +7,45 @@ class PostsController < ApplicationController
 		@post = Post.new
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def create
-		@post = Post.params([:post])
+		@post = Post.new(post_params)
 		if @post.save
 			flash[:success] = "Post has been published."
-			redirect_to :root
+			redirect_to '/posts'
 		else
 			render 'new'
 		end
 	end
 
-	def show
+	def edit
 		@post = Post.find(params[:id])
 	end
 
+	def update
+		@post = Post.find(params[:id])
+        if @post.update_attributes(post_params)
+            redirect_to posts_path, :notice=>"Your post is updated!"
+	    else
+            render "edit"
+        end 
+    end
+
+	def destroy
+	    @post = Post.find(params[:id])
+        if @post.destroy
+            redirect_to posts_path, :notice=> "Your post has been deleted!"
+        end 
+	end
+
+	def dashboard
+		@posts = Post.all
+	end
+
 	def post_params
-      	params.require(:post).permit(:title, :content, :type)
+      	params.require(:post).permit(:title, :content)
     end
 end
--- valayism.com/config/routes.rb
+++ valayism.com/config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  get '/_dashboard', to: 'posts#dashboard' 
   resources :posts
   # Example resource route with options:
   #   resources :products do
